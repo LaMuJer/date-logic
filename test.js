3255856525dd01fd018296eaf2ac4cd08f684b7d
laMuJer <lareina1467.com>
2023-01-30 23:35:51 +0630

    dates errror

--- test.js
+++ test.js
@@ -1,40 +1,40 @@
 function getDates(startDate, endDate) {
-        const dates = [];
-        let currentDate = startDate;
-        while (currentDate <= endDate) {
-          dates.push(new Date(currentDate));
-          currentDate.setDate(currentDate.getDate() + 1);
-        }
-        return dates;
-      }
-      const start = new Date("2022-11-11");
-      const end = new Date("2023-01-08");
-      const dates = getDates(start, end);
+  const dates = [];
+  let currentDate = startDate;
+  while (currentDate <= endDate) {
+    dates.push(new Date(currentDate));
+    currentDate.setDate(currentDate.getDate() + 1);
+  }
+  return dates;
+}
+const start = new Date("2022-11-11");
+const end = new Date("2023-01-08");
+const dates = getDates(start, end);
 
-      const assignOne = { A: "Off", B: "Morning", C: "Night", D: "Off" };
-      const assignTwo = { A: "Morning", B: "Off", C: "Off", D: "Night" };
-      const assignThree = { A: "Off", B: "Night", C: "Morning", D: "off" };
-      const assignFour = { A: "Night", B: "Off", C: "Off", D: "Morning" };
+const assignOne = { A: "Off", B: "Morning", C: "Night", D: "Off" };
+const assignTwo = { A: "Morning", B: "Off", C: "Off", D: "Night" };
+const assignThree = { A: "Off", B: "Night", C: "Morning", D: "off" };
+const assignFour = { A: "Night", B: "Off", C: "Off", D: "Morning" };
 
-      let count = 1;
-      let i = 1;
-      const data = [];
-      for (let i = 1; i < dates.length; i++) {
-        if (count < 3) {
-          data.push({ date: dates[i], assign: assignOne });
-          count++;
-        } else if (count < 5) {
-          data.push({ date: dates[i], assign: assignTwo });
-          count++;
-        } else if (count < 7) {
-          data.push({ date: dates[i], assign: assignThree });
-          count++;
-        } else if (count == 8) {
-          data.push({ date: dates[i], assign: assignFour });
-          count = 1;
-        } else {
-          data.push({ date: dates[i], assign: assignFour });
-          count++;
-        }
-      }
-      console.log(data);
+let count = 1;
+let i = 1;
+const data = [];
+for (let i = 1; i < dates.length; i++) {
+  if (count < 3) {
+    data.push({ date: dates[i], assign: assignOne });
+    count++;
+  } else if (count < 5) {
+    data.push({ date: dates[i], assign: assignTwo });
+    count++;
+  } else if (count < 7) {
+    data.push({ date: dates[i], assign: assignThree });
+    count++;
+  } else if (count == 8) {
+    data.push({ date: dates[i], assign: assignFour });
+    count = 1;
+  } else {
+    data.push({ date: dates[i], assign: assignFour });
+    count++;
+  }
+}
+console.log(data);
